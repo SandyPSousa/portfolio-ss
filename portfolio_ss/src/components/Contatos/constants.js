3d8340b4297a2add2d1dd0ae2b72217717031b09
Sandy Pereira de Sousa <141288740+SandyPSousa@users.noreply.github.com>
2024-09-02 18:04:06 +0000

    Edição do Título

--- portfolio_ss/src/components/Contatos/constants.js
+++ portfolio_ss/src/components/Contatos/constants.js
@@ -1,6 +1,28 @@
+// export const contatos = [
+//   { id: 1, name: "Facebook" },
+//   { id: 2, name: "Twitter" },
+//   { id: 3, name: "LinkedIn" },
+//   { id: 4, name: "Instagram" },
+// ];
 export const contatos = [
-  { id: 1, name: "Facebook" },
-  { id: 2, name: "Twitter" },
-  { id: 3, name: "LinkedIn" },
-  { id: 4, name: "Instagram" },
+  {
+    id: 1,
+    name: "GitHub",
+    url: "https://github.com/SandyPSousa",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    url: "www.linkedin.com/in/sandy-p-sousa",
+  },
+  {
+    id: 3,
+    name: "Twitter",
+    url: "https://twitter.com/user",
+  },
+  {
+    id: 4,
+    name: "Instagram",
+    url: "https://instagram.com/user",
+  },
 ];
--- portfolio_ss/src/components/Contatos/index.jsx
+++ portfolio_ss/src/components/Contatos/index.jsx
@@ -1,3 +1,23 @@
+// import Section from "../Section";
+// import styles from "./styles.module.css";
+// import { contatos } from "./constants";
+
+// export default function Contatos() {
+//   return (
+//     <Section id="contacts" title="Contatos">
+//       <ul className={styles.contacts_list}>
+//         {contatos.map((contato) => (
+//           <li key={contato.id}>
+//             <span className="material-symbols-outlined">link</span>
+//             <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+//               {contato.name}
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+//     </Section>
+//   );
+// }
 import Section from "../Section";
 import styles from "./styles.module.css";
 import { contatos } from "./constants";
@@ -9,7 +29,7 @@ export default function Contatos() {
         {contatos.map((contato) => (
           <li key={contato.id}>
             <span className="material-symbols-outlined">link</span>
-            <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+            <a href={contato.url} target="_blank" rel="noopener noreferrer">
               {contato.name}
             </a>
           </li>
@@ -18,3 +38,4 @@ export default function Contatos() {
     </Section>
   );
 }
+
