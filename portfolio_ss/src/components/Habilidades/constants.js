409ebb5b4bedafed4a0ef3f6dbcdee26661efbbf
Sandy Pereira de Sousa <141288740+SandyPSousa@users.noreply.github.com>
2024-09-02 17:34:55 +0000

    Mudança no nível das habilidades

--- portfolio_ss/src/components/Habilidades/constants.js
+++ portfolio_ss/src/components/Habilidades/constants.js
@@ -1,8 +1,8 @@
 export const mySkills = [
-  { id: 1, title: "HTML", stars: 5 },
-  { id: 2, title: "CSS", stars: 5 },
-  { id: 3, title: "JAVASCRIPT", stars: 5 },
-  { id: 4, title: "REACT", stars: 5 },
-  { id: 5, title: "PYTHON", stars: 5 },
-  { id: 6, title: "MYSQL", stars: 5 },
+  { id: 1, title: "HTML", stars: 4 },
+  { id: 2, title: "CSS", stars: 4 },
+  { id: 3, title: "JAVASCRIPT", stars: 3 },
+  { id: 4, title: "REACT", stars: 3 },
+  { id: 5, title: "PYTHON", stars: 3 },
+  { id: 6, title: "MYSQL", stars: 3 },
 ];
