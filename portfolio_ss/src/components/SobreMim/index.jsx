bbeca7d111845564cd4d188782a2d5d290fbb905
Sandy Pereira de Sousa <141288740+SandyPSousa@users.noreply.github.com>
2024-09-02 16:56:58 +0000

    Atualizando section "Sobre mim"

--- portfolio_ss/src/components/SobreMim/index.jsx
+++ portfolio_ss/src/components/SobreMim/index.jsx
@@ -4,10 +4,12 @@ export default function SobreMim() {
   return (
     <Section id="about-me" title="Sobre mim">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem obcaecati
-        illo aut aperiam voluptate incidunt cumque sequi? Saepe earum, rem qui
-        perferendis iste nam architecto, veritatis tempore minus omnis
-        consequatur.
+        Sou estudante de Engenharia de Software apaixonada por Cybersegurança.
+        Venho explorando Frontend com JavaScript, React, HTML e CSS, além de
+        Backend com SQL e Python. Recentemente, aprofundei meus conhecimentos em
+        análise de dados e modelagem de bancos de dados e busco combinar minhas
+        habilidades em desenvolvimento web com práticas de segurança para criar
+        aplicações seguras e inovadoras.
       </p>
     </Section>
   );
